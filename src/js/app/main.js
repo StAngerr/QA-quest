fd8cc205166318184199e86facce182528b2aab0
Taras Stekhnovych <violet.infierno@gmail.com>
2015-04-21 16:32:08 +0300

    Stick game started

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -2,8 +2,33 @@ define(function (require) {
 	var Quest = require('src/js/app/qaquest.js');
 	var quest = new Quest();
 	var $ = require('jquery');
-
+	QQQ('popup.html');
     $('.startBtn').on('click', function() {
         quest.startQuest();
     });
 });
+
+
+function QQQ(varr) {
+	$.ajax({
+        url: 'src/templates/' + varr,
+        method: 'GET',
+        async: false,
+        success: function(data) {
+            $('#mainContent').prepend(data);
+            addGameField();
+        }
+    });
+}
+
+function addGameField() {
+	$.ajax({
+        url: 'src/templates/' + 'sticksGameTmpl.html',
+        method: 'GET',
+        async: false,
+        success: function(data) {
+            $('.popup').append(data);
+         
+        }
+    });
+}
