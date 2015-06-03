ea386cf13f0d02fb0ec86e4aa4fd179c1ad46cc7
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-06-03 16:51:54 +0300

    sky is blue

--- src/js/stages/stage1.js
+++ src/js/stages/stage1.js
@@ -1,5 +1,5 @@
-define(function(require) {
-    var Stage = require('src/js/Stage.js');    
+define(function (require) {
+    var Stage = require('src/js/Stage.js');
     var stage1 = new Stage('stage1Tmpl.html');
     var $ = require('jquery');
     var DragNDrop = require('src/js/dragndrop.js');
@@ -11,60 +11,63 @@ define(function(require) {
     var that;
     var counter = 0;
 
-    stage1.initEvents = function() {
+    stage1.initEvents = function () {
+        $('#inventory').show();
         $('#hero').show();
-        $('.door').on('click', function(event) {
-            if(manState == 'stand') stage1.moveToDoor();
+        $('.door').on('click', function (event) {
+            if (manState == 'stand') stage1.moveToDoor();
         });
     };
 
-    stage1.moveToDoor = function() {
-        changeManState(); 
-        if($('.man').css('left') !== '750px') {
-            $('.man').animate({'left' : '750'}, 2000, openWordGame);
+    stage1.moveToDoor = function () {
+        changeManState();
+        if ($('.man').css('left') !== '750px') {
+            $('.man').animate({
+                'left': '750'
+            }, 2000, openWordGame);
         } else {
             openWordGame();
-        }  
+        }
     };
 
     stage1.dragNdrop = new DragNDrop();
-    
-   
+
+
     function openWordGame() {
         var stage_content = {
             taskDescription: 'Your task is to make a right word with all these letters. You should move them to text field',
-            letters : ['e', 'm', 'i', 'c', 'a', 't', 's', 'p', 'c', 'r']
-        }; 
-        if(wordGameStatus == 'unfinished') {
+            letters: ['e', 'm', 'i', 'c', 'a', 't', 's', 'p', 'c', 'r']
+        };
+        if (wordGameStatus == 'unfinished') {
             stage1.getTmpl('popupFrameTmpl.html');
-            stage1.getTmpl('stage1WordGameTmpl.html','.popup', stage_content);
+            stage1.getTmpl('stage1WordGameTmpl.html', '.popup', stage_content);
             $('#sendWord').on('click', showWord);
             /*Drag n drop*/
             var leafes = $('.letters');
             var wordSpot = $('.makeWord')[0];
             var fieldToDrop = $('.all-letters')[0];
-            $('.letters').attr("draggable","true");
+            $('.letters').attr("draggable", "true");
             wordSpot.addEventListener('dragover', stage1.dragNdrop.allowDrop);
-            wordSpot.addEventListener('drop', function(event) {
+            wordSpot.addEventListener('drop', function (event) {
                 event.preventDefault();
-                event.stopPropagation();                      
+                event.stopPropagation();
                 counter++;
-                drop(event);                
+                drop(event);
                 word += that.children().text();
-                return false;  
-                        
+                return false;
+
             });
             fieldToDrop.addEventListener('dragover', stage1.dragNdrop.allowDrop);
-            fieldToDrop.addEventListener('drop', function(event) {
+            fieldToDrop.addEventListener('drop', function (event) {
 
                 event.preventDefault();
-                event.stopPropagation();  
-                counter --;
-                drop(event);                      
-                word =  $('.makeWord').children().children(1).text();             
-                return false;           
+                event.stopPropagation();
+                counter--;
+                drop(event);
+                word = $('.makeWord').children().children(1).text();
+                return false;
             });
-              
+
             for (var i = 0; i < leafes.length; i++) {
                 leafes[i].addEventListener("dragstart", stage1.dragNdrop.drag);
                 leafes[i].addEventListener("dragover", stage1.dragNdrop.over);
@@ -76,49 +79,62 @@ define(function(require) {
         }
     };
 
-    stage1.finishStage = function() {
+    stage1.finishStage = function () {
         $('#mainSection').trigger('main:stageFinished');
     };
 
-    function showWord() {        
+    function showWord() {
         $('#stage1Popup1').remove();
-         $('.popupWrap').remove();
-        $('.detail-1').show();         
-        setTimeout(sendDnDWord, 2000); 
+        $('.popupWrap').remove();
+        $('.detail-1').show();
+        setTimeout(sendDnDWord, 2000);
 
-        
+
     };
 
     function sendDnDWord(event) {
         // add the first inventory
-        $('#inventory').trigger('inventory:addItem', {data:'.detail-1'});  
+        $('#inventory').trigger('inventory:addItem', {
+            data: '.detail-1'
+        });
         $('.item.detail-1').remove();
 
-        $('.door').css('opacity','0');
-        $('.totalLevel').css({'display': 'block', 'opacity' : '1'});
-        var myVar = setInterval(function() { 
-            /*remove leafes*/
-            if($('.leafes')) $('.leafes').remove();
-            /*flashing box border animation*/
-            $('.totalLevel').animate({'opacity': '1'}, 400, function() { $('.totalLevel').animate({'opacity' : '0'}, 400); } );
-        }, 830);
-        $('.totalLevel').on('click', function() {
-            clearInterval(myVar);
-           if(manState == 'stand' && flowGameStatus == 'unfinished') moveToBox();
+        $('.door').css('opacity', '0');
+        $('.totalLevel').css({
+            'display': 'block',
+            'opacity': '1'
         });
-       
+        var myVar = setInterval(function () {
+            /*remove leafes*/
+            if ($('.leafes')) $('.leafes').remove();
+            /*flashing box border animation*/
+            $('.totalLevel').animate({
+                'opacity': '1'
+            }, 400, function () {
+                $('.totalLevel').animate({
+                    'opacity': '0'
+                }, 400);
+            });
+        }, 830);
+        $('.totalLevel').on('click', function () {
+            clearInterval(myVar);
+            if (manState == 'stand' && flowGameStatus == 'unfinished') moveToBox();
+        });
+
         wordGameStatus = 'finished';
     };
 
     function moveToBox() {
         changeManState();
-        $('.popupWrap').remove();/*!!!*/
-        $('.man').animate({'left' : '450'}, 1000, function() {
+        $('.popupWrap').remove(); /*!!!*/
+        $('.man').animate({
+            'left': '450'
+        }, 1000, function () {
             $('.totalLevel').remove();
             stage1.getTmpl('popupFrameTmpl.html');
             $('.popupWrap').append('<div id="wade_main_div" width="800" height="600" tabindex="1"></div>'); /* set sizes*/
             wade.init('src/js/flow.js');
-            $('.popup').on('flowGameFinished', finishFlowGame); 
+            $('.popup').on('flowGameFinished', finishFlowGame);
             changeManState();
         });
     };
@@ -128,14 +144,18 @@ define(function(require) {
         $('.popupWrap').remove();
         $('.detail-2').show();
         flowGameStatus = 'finished';
-        setTimeout(closePopup, 2000); 
+        setTimeout(closePopup, 2000);
     };
 
     function closePopup(event) {
-        $('#inventory').trigger('inventory:addItem', {data:'.detail-2'});  
+        $('#inventory').trigger('inventory:addItem', {
+            data: '.detail-2'
+        });
         $('.item.detail-2').remove();
-        $('.totalLevel').css({'opacity':'1'});
-        
+        $('.totalLevel').css({
+            'opacity': '1'
+        });
+
     };
 
 
@@ -143,47 +163,47 @@ define(function(require) {
         event.preventDefault();
         event.stopPropagation();
 
-        that = stage1.dragNdrop.data; 
-        
-        var data =  '<figure class="letters" draggable="true"> ' + event.dataTransfer.getData('text/html') + '</figure>';
-        var html =  $(event.target).html();
+        that = stage1.dragNdrop.data;
+
+        var data = '<figure class="letters" draggable="true"> ' + event.dataTransfer.getData('text/html') + '</figure>';
+        var html = $(event.target).html();
         var targetNodeName = $(event.target)[0].nodeName;
-        $('.makeWord').css ('background-color', '#fff'); 
+        $('.makeWord').css('background-color', '#fff');
         if ($(event.target)[0].className != $(that).parent()[0].className) {
-            if ( $(event.target).parent().parent()[0].className != $(that).parent()[0].className) {
-                if( targetNodeName == 'SPAN' || targetNodeName == "IMG") { 
-                        html =  $(event.target).parent().parent().html()       
-                        $(event.target).parent().parent().html(html + data);          
+            if ($(event.target).parent().parent()[0].className != $(that).parent()[0].className) {
+                if (targetNodeName == 'SPAN' || targetNodeName == "IMG") {
+                    html = $(event.target).parent().parent().html()
+                    $(event.target).parent().parent().html(html + data);
                 } else if (targetNodeName == 'FIGURE') {
-                    html =  $(event.target).parent().html();
-                     $(event.target).parent().html(html + data);                      
+                    html = $(event.target).parent().html();
+                    $(event.target).parent().html(html + data);
                 } else {
-                    $(event.target).html(html + data); 
-                }       
-                that.remove(); 
+                    $(event.target).html(html + data);
+                }
+                that.remove();
                 var leafes = $('.letters');
-                  for (var i = 0; i < leafes.length; i++) {
-                        leafes[i].addEventListener("dragstart",stage1.dragNdrop.drag);
-                        leafes[i].addEventListener("dragover", stage1.dragNdrop.over);
-                        leafes[i].addEventListener("dragleave", stage1.dragNdrop.leave);
-                    };
-        
-                if (counter === 10) {          
+                for (var i = 0; i < leafes.length; i++) {
+                    leafes[i].addEventListener("dragstart", stage1.dragNdrop.drag);
+                    leafes[i].addEventListener("dragover", stage1.dragNdrop.over);
+                    leafes[i].addEventListener("dragleave", stage1.dragNdrop.leave);
+                };
+
+                if (counter === 10) {
                     $('#sendWord').show();
                     return false;
                 }
             }
-                    
-        } else {     
-            that.css('opacity', '1'); 
-                    return false;
-            
-        }       
+
+        } else {
+            that.css('opacity', '1');
+            return false;
+
+        }
     };
 
     function changeManState() {
-        (manState == 'stand') ? manState = 'moving' : manState = 'stand';
+        (manState == 'stand') ? manState = 'moving': manState = 'stand';
     };
 
-   return stage1;
+    return stage1;
 });
--- src/js/stages/stage3.js
+++ src/js/stages/stage3.js
@@ -1,214 +1,205 @@
-define(function(require) {
-    var Stage = require('src/js/Stage.js');    
+define(function (require) {
+    var Stage = require('src/js/Stage.js');
     var stage3 = new Stage('stage3Tmpl.html');
     var $ = require('jquery');
     var wade = require('wade');
 
-    stage3.initEvents = function() {
-    	/* it temporarily until standard control is not define*/
+    stage3.initEvents = function () {
+        /* it temporarily until standard control is not define*/
 
-   				var heroChange = {
-          left : '40px',
-          top:'530px',
-          height: '130px'
+        var heroChange = {
+            left: '40px',
+            top: '530px',
+            height: '130px'
         };
-        $('#hero').css( heroChange );
+        $('#hero').css(heroChange);
         $('#hero').show();
-        $('#stage3').on('click', function(e){
-        	var x = e.pageX;
+        $('#stage3').on('click', function (e) {
+            var x = e.pageX;
             var y = e.pageY;
 
-          if(x <= 811 ) return false;
+            if (x <= 811) return false;
 
-          $('.man').animate({'left' : '285'}, 1300, function() {
-          	stage3.getTmpl('popupFrameTmpl.html');
-       			stage3.getTmpl('stage3SticksGameTmpl.html','.popup');
-       			var newGame = new StickGame();
-						newGame.startGame();
-						$('.pickStick').on('click', newGame.playerPicks);
-          });         	
+            $('.man').animate({
+                'left': '285'
+            }, 1300, function () {
+                stage3.getTmpl('popupFrameTmpl.html');
+                stage3.getTmpl('stage3SticksGameTmpl.html', '.popup');
+                var newGame = new StickGame();
+                newGame.startGame();
+                $('.pickStick').on('click', newGame.playerPicks);
+            });
         });
-       
-		/*------------------------------------------*/
+
+        /*------------------------------------------*/
     };
 
 
 
-	function bubbleStart() {
-
-		
-		  $('#stage3').append('<div class="bubbles-popup"><div id="wade_main_div" width="800" height="600" tabindex="1" margin="0"></div></div>'); /* set sizes*/
-       wade.init('src/js/lib/wade_src/bubbles.js');    
-       
+    function bubbleStart() {
+        $('#stage3').append('<div class="bubbles-popup"><div id="wade_main_div" width="800" height="600" tabindex="1" margin="0"></div></div>'); /* set sizes*/
+        wade.init('src/js/lib/wade_src/bubbles.js');
     };
 
-    function bubbleFinish(){
-    	
-    	
-    	$('.detail-6').show();
-    	setTimeout(function () {
-    		 $('#inventory').trigger('inventory:addItem',{data:'.detail-6'}); 
-    		$('#mainSection').trigger('main:stageFinished'); 
-    	}, 2500);
-    	 
-    }
 
-	function StickGame() {
-		var currentGame = this;
-		currentGame.firstPlayer = {
-			name : 'player',
-			sticks : 0
-		};
-		currentGame.secondPlayer = {
-			name : 'computer',
-			sticks : 0
-		};	
-		currentGame.whoseTurn = currentGame.firstPlayer.name;
 
-		currentGame.startGame = function() {
-			currentGame.nextPick();
-		};
+    function StickGame() {
+        var currentGame = this;
+        currentGame.firstPlayer = {
+            name: 'player',
+            sticks: 0
+        };
+        currentGame.secondPlayer = {
+            name: 'computer',
+            sticks: 0
+        };
+        currentGame.whoseTurn = currentGame.firstPlayer.name;
 
-		currentGame.nextPick = function() {		
-			if( !(checkGameStatus()) ) {
-				$('.pickStick').addClass('disabled') /*DELETE*/
-				$('.pickStick').removeAttr('disabled'); /* REMOVE IT*/
-	 			currentGame.finishGame();
-				return;
-			}
-			if(currentGame.whoseTurn == 'computer') {
-				currentGame.computerPicks();
-			}
-		};
+        currentGame.startGame = function () {
+            currentGame.nextPick();
+        };
 
-		currentGame.finishGame = function() {
-			var winner = (currentGame.whoseTurn == 'player') ? 'computer' : 'player';
-			printWhoWon(winner);
-			/*CHECK WHO IS WINNER*/
-			$('.popupWrap').remove();/*!!!*/ 
-			$('.detail-5').show();
-			setTimeout(function(){
-				 $('#inventory').trigger('inventory:addItem',{data:'.detail-5'}); 
-				 bubbleStart();
-				  //$('#mainSection').trigger('main:stageFinished');
-			}, 2500);
-		};
+        currentGame.nextPick = function () {
+            if (!(checkGameStatus())) {
+                $('.pickStick').addClass('disabled') /*DELETE*/
+                $('.pickStick').removeAttr('disabled'); /* REMOVE IT*/
+                currentGame.finishGame();
+                return;
+            }
+            if (currentGame.whoseTurn == 'computer') {
+                currentGame.computerPicks();
+            }
+        };
 
-		currentGame.playerPicks = function(event) {
-			var sticks = event.target.name;
-			if(canPick(sticks)) {
-				currentGame.firstPlayer.sticks += pickSticks(sticks);
-				changeTurn();
-				changeBtnState(); /*DELETE*/
-				currentGame.nextPick();
-			} else {
-				alert('not enough sticks left');
-			}
-		};
+        currentGame.finishGame = function () {
+            var winner = (currentGame.whoseTurn == 'player') ? 'computer' : 'player';
+            printWhoWon(winner);
+            /*CHECK WHO IS WINNER*/
+            $('.popupWrap').remove(); /*!!!*/
+            $('.detail-5').show();
+            setTimeout(function () {
+                $('#inventory').trigger('inventory:addItem', {
+                    data: '.detail-5'
+                });
+                bubbleStart();
+            }, 2500);
+        };
 
-		currentGame.computerPicks = function() {
-			var max = 3;
-			var min = 1;
-			var sticks = Math.floor(Math.random() * max) + min;
-			/*a little bit intellect  for computer. if left less than 3 sticks take them all*/
-			var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length;
-			if(sticksLeft <= 3) {
-				sticks = sticksLeft;
-			} else if(sticksLeft <= 6) {
-				switch(sticksLeft) {
-					case 8: 
-						sticks = 1;
-						break;
-					case 7:
-						sticks = 3;
-						break;
-					case 6:
-						sticks = 2;
-						break;
-					case 5: 
-						sticks = 1;
-						break;
-				}
-			}
-			/* little pause when compute picks*/
-			var compPickDelay = setTimeout(function() {
-				currentGame.secondPlayer.sticks += pickSticks(sticks);
-				changeTurn();
-				currentGame.nextPick();
-				clearTimeout(compPickDelay);
-				changeBtnState(); /*DELETE*/
-			}, 1000);
-		};
-			/* this function checks if is it a game end*/
-		function checkGameStatus() {  
-			var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length;/* here must be strict path to sticks */
-			if(sticksLeft) {
-				return true;
-			}
-			return false;
-		};
+        currentGame.playerPicks = function (event) {
+            var sticks = event.target.name;
+            if (canPick(sticks)) {
+                currentGame.firstPlayer.sticks += pickSticks(sticks);
+                changeTurn();
+                changeBtnState(); /*DELETE*/
+                currentGame.nextPick();
+            } else {
+                alert('not enough sticks left');
+            }
+        };
 
-		function changeBtnState() {
-			var buttons = $('.pickStick');
+        currentGame.computerPicks = function () {
+            var max = 3;
+            var min = 1;
+            var sticks = Math.floor(Math.random() * max) + min;
+            /*a little bit intellect  for computer. if left less than 3 sticks take them all*/
+            var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length;
+            if (sticksLeft <= 3) {
+                sticks = sticksLeft;
+            } else if (sticksLeft <= 6) {
+                switch (sticksLeft) {
+                case 8:
+                    sticks = 1;
+                    break;
+                case 7:
+                    sticks = 3;
+                    break;
+                case 6:
+                    sticks = 2;
+                    break;
+                case 5:
+                    sticks = 1;
+                    break;
+                }
+            }
+            /* little pause when compute picks*/
+            var compPickDelay = setTimeout(function () {
+                currentGame.secondPlayer.sticks += pickSticks(sticks);
+                changeTurn();
+                currentGame.nextPick();
+                clearTimeout(compPickDelay);
+                changeBtnState(); /*DELETE*/
+            }, 1000);
+        };
+        /* this function checks if is it a game end*/
+        function checkGameStatus() {
+            var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length; /* here must be strict path to sticks */
+            if (sticksLeft) {
+                return true;
+            }
+            return false;
+        };
 
-			if($(buttons).hasClass('disabled')) { /*disable with css !!!!!!!!! */
-				$(buttons).removeAttr('disabled');
-				$(buttons).removeClass('disabled');
-			} else {
-				$(buttons).attr('disabled','disabled');
-				$(buttons).addClass('disabled');
-			}
-		};
+        function changeBtnState() {
+            var buttons = $('.pickStick');
 
-		function changeTurn() {
-			if(currentGame.whoseTurn == 'player') {
-				currentGame.whoseTurn = currentGame.secondPlayer.name;
-				visualizeTurn();	
-			} else {
-				currentGame.whoseTurn = currentGame.firstPlayer.name;
-				visualizeTurn();
-			}
-		};
+            if ($(buttons).hasClass('disabled')) { /*disable with css !!!!!!!!! */
+                $(buttons).removeAttr('disabled');
+                $(buttons).removeClass('disabled');
+            } else {
+                $(buttons).attr('disabled', 'disabled');
+                $(buttons).addClass('disabled');
+            }
+        };
 
-		function canPick(sticksCount) {
-			var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length;/* here must be strict path to sticks */
-			
-			if(sticksCount <= sticksLeft) {
-				return true;
-			}
-			return false;
-		};
+        function changeTurn() {
+            if (currentGame.whoseTurn == 'player') {
+                currentGame.whoseTurn = currentGame.secondPlayer.name;
+                visualizeTurn();
+            } else {
+                currentGame.whoseTurn = currentGame.firstPlayer.name;
+                visualizeTurn();
+            }
+        };
 
-		function pickSticks(sticksCount) {
-			var sticksLeft = $('.mainSection .stick');
+        function canPick(sticksCount) {
+            var sticksLeft = $('#stickGameWrapp  .sticksSection  .stick').length; /* here must be strict path to sticks */
 
-			for (var i = sticksCount; i >= 1; i--) {
-				var curStick = $(sticksLeft[i - 1]);
-				$(curStick).remove();
-				addStickBlock(currentGame.whoseTurn, curStick);
-			}
-			return sticksCount;
-		};
+            if (sticksCount <= sticksLeft) {
+                return true;
+            }
+            return false;
+        };
 
-		function visualizeTurn() {
-			if(currentGame.whoseTurn == 'player') {
-				$('.cpuPlace').removeClass('activeCpu');
-				$('.playerPlace').addClass('activePlayer');
-			} else {
-				$('.playerPlace').removeClass('activePlayer');
-				$('.cpuPlace').addClass('activeCpu');
-			}
-		};
+        function pickSticks(sticksCount) {
+            var sticksLeft = $('.mainSection .stick');
 
-		function addStickBlock(player, stick) {
-			var where = (player == 'player') ? '.playerPlace' : '.cpuPlace';
-			$(where + ' > .pickedSticks').append(stick);
-		};
+            for (var i = sticksCount; i >= 1; i--) {
+                var curStick = $(sticksLeft[i - 1]);
+                $(curStick).remove();
+                addStickBlock(currentGame.whoseTurn, curStick);
+            }
+            return sticksCount;
+        };
 
-		function printWhoWon(winner) {
-			$('.sticksSection > *').remove();
-			$('.sticksSection').append('<h1>' + winner  + ' won! </h1>')
-		};
-	};
+        function visualizeTurn() {
+            if (currentGame.whoseTurn == 'player') {
+                $('.cpuPlace').removeClass('activeCpu');
+                $('.playerPlace').addClass('activePlayer');
+            } else {
+                $('.playerPlace').removeClass('activePlayer');
+                $('.cpuPlace').addClass('activeCpu');
+            }
+        };
+
+        function addStickBlock(player, stick) {
+            var where = (player == 'player') ? '.playerPlace' : '.cpuPlace';
+            $(where + ' > .pickedSticks').append(stick);
+        };
+
+        function printWhoWon(winner) {
+            $('.sticksSection > *').remove();
+            $('.sticksSection').append('<h1>' + winner + ' won! </h1>')
+        };
+    };
     return stage3;
 });
