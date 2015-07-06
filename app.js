3b0fa6f59afc51869693eed723c64ad2010fdfce
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-07-06 17:51:20 +0300

    added questions.json

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ app.use(bodyParser.json());
 
 app
 	.post('/time', function(req, res) {
-		console.log('POSTING     ' + req.body.seconds);
+		console.log(req.body.seconds);
 		res.end();
 	})
 	.get('/time', function (req, res) {
 		var  seconds = 0;
+
 		res.json({seconds: seconds});
 	});
 
 app.get('/getStage', function(req, res) {
 	var stage = 0;
 
-	//serverTime = 0;
 	res.json({ stage: stage })
 	//res.status(404).end();
 });
@@ -37,26 +37,28 @@ app.post('/gameResult', function(req, res) {
 app
 	.get('/wordGame', function(req, res) {
 		var questions = [
-				{ 
-					'question' : 'what is my name?',
-					'answer' : 'dfhgjkdfhgkjfd'
-				},
+				{
+    "question":"The process of confirming that a component, system or person complies with its specified requirements, e.g., by passing an exam",
+    "answer": "Certification"
+  },
 				{ 
 					'question' : ' what is my color?',
 					'answer' : 'green'
 				}];
 		var questionToSend = { 
-					'question' : 'what is my name?',
-					'letters' : ["a", "b", "c", "d", "e", "e", "e", "e", "e", "e"]
+					'question' : 'The process of confirming that a component, system or person complies with its specified requirements, e.g., by passing an exam',
+					'letters' : ["i","r", "t", "c", "c", "e", "i", "a", "t", "i", "f", "n","o"]
 				};
 		res.json({question: questionToSend});
 	})  
 	.post('/wordGame', function(req, res) {
 		var word = req.body.word;
+		console.log(word);
 		res.status(200).end();
 	});
 
 	app.post('/pictureID', function(req, res) {
+		console.log(req.body.picture);
 		res.status(200).end();
 	});
 
