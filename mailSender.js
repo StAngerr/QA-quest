6a1a88500936508d66be7d1ca6b513bad6a81982
Albina.Kremlovska <albina.kremlovska@thomsonreuters.com>
2016-04-19 15:59:30 +0300

    mail tmpl added

--- mailSender.js
+++ mailSender.js
@@ -5,8 +5,8 @@ var emailOptions = {
     service: 'Gmail',
     debug: true,
     auth: {
-        user: '',
-        pass: ''
+        user: 'questtestepam@gmail.com',
+        pass: 'questTest123'
     }
 };
 
@@ -26,10 +26,16 @@ function sendEmails(address, users) {
 	var transporter  = nodemailer.createTransport("SMTP", emailOptions);
 	for (var i = 0; i < address.length; i++) {
 		var email = {
-		    from: 'box4alya@gmail.com',
+		    from: 'fenomenseed1@gamail.com',
 		    to: address[i],
 		    subject: 'QA quest',
-		    html: '<div style="border: 2px solid #DDD; text-align: center;"><h1>Welcome!</h1>\nBro  :D\n <div>Your credentials:</div>\n <h3>Username: ' + users[i].username + '\n</h3><h3> Password: ' + users[i].password + '\n</h3></div>'
+		    html : '<table border="0" cellpadding="0" cellspacing="0" style="margin:0; padding:0"><tr><td style="border: 5px solid rgb(90,74,66); bgcolor: rgb(140,172,79); background: rgb(140,172,79); padding:50px"><center style="max-width: 480px; width: 100%;">'+
+        '<table cellpadding="0" cellspacing="0" style="border: 1px solid rgb(90,74,66); margin:0; padding:0"><tr><td style="background: rgb(220,230,202); bgcolor: rgb(220,230,202); padding:20px"><div style=" font-family: \'MS Sans Serif\', Geneva, sans-serif; font-style:italic; font-size: 1.6em; line-height: 1.4em; text-align:center">'+
+		    '<h3 style="color:rgb(226,95,93); font-size: 1.7em; text-align:center">Welcome to QAQuest!</h3> Hello,  '  + 
+		     users[i].username + '. <br/><br/>Your account\'s all set up and you\'re ready to start discovering <br/> QAQuest game - just quickly verify your credentials to sign in: <p style="margin-left:15%; text-align:left;"><span style="color:rgb(226,95,93);">Username: </span> ' + 
+		     users[i].username + '<br/><span style="color:rgb(226,95,93); ">Password: </span> ' + 
+		     users[i].password + '</p> If you\'re heading out to explore, please,  <a href="#">follow the link</a>  <br/><br/> <br/>Have fun out there, <br/> The QAQuest Team</div></td></tr></table>'+
+      '</center></td></tr></table>'
 		};
 		//transporter
 		transporter.sendMail(email, function(error, info){
