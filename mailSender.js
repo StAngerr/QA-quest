7941740486df97f05205c7007c44a9bb822344c6
Albina.Kremlovska <albina.kremlovska@thomsonreuters.com>
2016-04-19 16:12:56 +0300

    email style's  improvements

--- mailSender.js
+++ mailSender.js
@@ -29,9 +29,9 @@ function sendEmails(address, users) {
 		    from: 'fenomenseed1@gamail.com',
 		    to: address[i],
 		    subject: 'QA quest',
-		    html : '<table border="0" cellpadding="0" cellspacing="0" style="margin:0; padding:0"><tr><td style="border: 5px solid rgb(90,74,66); bgcolor: rgb(140,172,79); background: rgb(140,172,79); padding:50px"><center style="max-width: 480px; width: 100%;">'+
-        '<table cellpadding="0" cellspacing="0" style="border: 1px solid rgb(90,74,66); margin:0; padding:0"><tr><td style="background: rgb(220,230,202); bgcolor: rgb(220,230,202); padding:20px"><div style=" font-family: \'MS Sans Serif\', Geneva, sans-serif; font-style:italic; font-size: 1.6em; line-height: 1.4em; text-align:center">'+
-		    '<h3 style="color:rgb(226,95,93); font-size: 1.7em; text-align:center">Welcome to QAQuest!</h3> Hello,  '  + 
+		    html : '<table border="0" cellpadding="0" cellspacing="0" style="margin:0; padding:0"><tr><td style="border: 5px solid rgb(90,74,66); bgcolor: rgb(140,172,79); background: rgb(140,172,79); padding:50px"><center style="max-width: 550px; width: 100%;">'+
+        '<table cellpadding="0" cellspacing="0" style="border: 1px solid rgb(90,74,66); margin:0; padding:0"><tr><td style="background: rgb(220,230,202); bgcolor: rgb(220,230,202); padding: 20px 35px"><div style=" font-family: \'NotoSans Italic/Bold Italic\', Geneva, sans-serif; font-style:italic; color: rgb(90,74,66); font-size: 1.2em; line-height: 1.3em; text-align:center">'+
+		    '<h3 style="color:rgb(226,95,93); font-size: 1.2em; text-align:center">Welcome to QAQuest!</h3> Hello,  '  + 
 		     users[i].username + '. <br/><br/>Your account\'s all set up and you\'re ready to start discovering <br/> QAQuest game - just quickly verify your credentials to sign in: <p style="margin-left:15%; text-align:left;"><span style="color:rgb(226,95,93);">Username: </span> ' + 
 		     users[i].username + '<br/><span style="color:rgb(226,95,93); ">Password: </span> ' + 
 		     users[i].password + '</p> If you\'re heading out to explore, please,  <a href="#">follow the link</a>  <br/><br/> <br/>Have fun out there, <br/> The QAQuest Team</div></td></tr></table>'+
