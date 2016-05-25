52a9f17b4f5ba7f812539184d5372b6dbbd5bf02
Albina <Kremliovskyi@Mac-mini-Andrew-4.local>
2016-05-25 21:11:50 +0300

    normalize path

--- src/router/accountManaging.js
+++ src/router/accountManaging.js
@@ -10,7 +10,7 @@
         jsonToXls = require('json2xls');
 
     router.get('/manageStage', function(req, res) {
-        res.sendFile('/dataMngLogin.temp.html', {root: __dirname + '../../../ui-app' });
+        res.sendFile('/dataMngLogin.temp.html', {root: path.normalize(__dirname + '../../../ui-app')});
     });
     // make xls report
     // it'll be downloaded automatically
@@ -60,7 +60,7 @@
 
     //dataManageView
     router.get('/dataManageView', function(req, res) {
-        res.sendFile('dataManager.temp.html', {root: __dirname + '../../../ui-app' });
+        res.sendFile('dataManager.temp.html', {root: path.normalize(__dirname + '../../../ui-app') });
 
     });
     router.get('/resetResults', function(req, res) {
